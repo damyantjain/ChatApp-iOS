ccd4f538c7d2e61d58cb32ef8f36aba7c2560849
damyantjain <damyant9999@gmail.com>
2024-11-05 16:11:54 -0500

    Testing firebase connection

--- ChatApp-iOS/ChatApp-iOS/ViewController.swift
+++ ChatApp-iOS/ChatApp-iOS/ViewController.swift
@@ -5,15 +5,44 @@
 //  Created by Damyant Jain on 11/4/24.
 //
 
+import FirebaseFirestore
 import UIKit
 
 class ViewController: UIViewController {
 
+    let db = Firestore.firestore()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Task {
+            //await addUser()
+            await getAllUsers()
+        }
+    }
+    
+    func getAllUsers() async{
+        do {
+          let snapshot = try await db.collection("users").getDocuments()
+          for document in snapshot.documents {
+            print("\(document.documentID) => \(document.data())")
+          }
+        } catch {
+          print("Error getting documents: \(error)")
+        }
     }
 
+    func addUser() async {
+        do {
+            let ref = try await db.collection("users").addDocument(data: [
+                "first": "Ada",
+                "last": "Lovelace",
+                "born": 1815,
+            ])
+            print("Document added with ID: \(ref.documentID)")
+        } catch {
+            print("Error adding document: \(error)")
+        }
+    }
 
 }
-
