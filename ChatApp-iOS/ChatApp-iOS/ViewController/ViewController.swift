9f94b2ddb5ce3aeb0ffb5d7187d231ae855046b3
saniya16 <saniyaanklesaria16@gmail.com>
2024-11-06 20:11:48 -0500

    Added get all chats in console

--- ChatApp-iOS/ChatApp-iOS/ViewController/ViewController.swift
+++ ChatApp-iOS/ChatApp-iOS/ViewController/ViewController.swift
@@ -20,34 +20,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title="My Chats"
-        // Do any additional setup after loading the view.
         Task {
-            //await addUser()
             await getAllUsers()
         }
     }
     
     func getAllUsers() async{
         do {
-          let snapshot = try await db.collection("users").getDocuments()
+          let snapshot = try await db.collection("chats").getDocuments()
           for document in snapshot.documents {
             print("\(document.documentID) => \(document.data())")
           }
         } catch {
           print("Error getting documents: \(error)")
-        }
-    }
-
-    func addUser() async {
-        do {
-            let ref = try await db.collection("users").addDocument(data: [
-                "first": "Ada",
-                "last": "Lovelace",
-                "born": 1815,
-            ])
-            print("Document added with ID: \(ref.documentID)")
-        } catch {
-            print("Error adding document: \(error)")
         }
     }
 
