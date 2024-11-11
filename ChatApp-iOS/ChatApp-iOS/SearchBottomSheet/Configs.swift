07b7dbc0968a53ae04d309061505edd68839093c
gautamraju <raju.ga@northeastern.edu>
2024-11-11 11:07:42 -0500

    Added all the Bottom sheet code

---
+++ ChatApp-iOS/ChatApp-iOS/SearchBottomSheet/Configs.swift
@@ -0,0 +1,11 @@
+//
+//  Configs.swift
+//  BottomSheetViewDemo
+//
+//  Created by Sakib Miazi on 6/13/23.
+//
+
+import Foundation
+class Configs{
+    static let searchTableViewID = "searchTableViewID"
+}
--- ChatApp-iOS/ChatApp-iOS/ViewController/ViewController.swift
+++ ChatApp-iOS/ChatApp-iOS/ViewController/ViewController.swift
@@ -12,6 +12,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let searchSheetController = SearchBottomSheetController()
+    var searchSheetNavController: UINavigationController!
+    
     let db = Firestore.firestore()
     var landView = LandingView()
     var loggedInUser = User(email: "", name: "")
@@ -59,6 +62,10 @@ class ViewController: UIViewController {
         landView.allChatsTableView.dataSource = self
         landView.allChatsTableView.separatorStyle = .singleLine
     }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
     func getAllChats() async {
         do {
@@ -88,8 +95,28 @@ class ViewController: UIViewController {
         }
     }
     @objc func onAddBarButtonTapped() {
-
+        setupSearchBottomSheet()
+        searchSheetController.loggedInUser = loggedInUser
+        // changed here
+        searchSheetController.navigationController1 = self.navigationController
+        present(searchSheetNavController, animated: true)
     }
+    
+    
+    func setupSearchBottomSheet(){
+
+  
+        searchSheetNavController = UINavigationController(rootViewController: searchSheetController)
+        
+       
+        searchSheetNavController.modalPresentationStyle = .pageSheet
+        
+        if let bottomSearchSheet = searchSheetNavController.sheetPresentationController{
+            bottomSearchSheet.detents = [.medium(), .large()]
+            bottomSearchSheet.prefersGrabberVisible = true
+        }
+    }
+    
     func logout() {
         let logoutAlert = UIAlertController(
             title: "Logging out!", message: "Are you sure want to log out?",
